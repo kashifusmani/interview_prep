ff0d584dc3c90b490a3c9dc8a8649c4efee03657
Usmani <kusmani@ebay.com>
2020-09-08 18:14:27 -0400

    OMW

--- punchh/app/data_loader.py
+++ punchh/app/data_loader.py
@@ -1,43 +1,68 @@
 import gzip
 from json import loads, JSONDecodeError
-import MySQLdb
+import psycopg2
+from psycopg2 import DatabaseError
+from psycopg2.extras import execute_batch
 import argparse
 from datetime import datetime
 from time import sleep
 
-date_fmt ='%Y-%m-%d'
+date_fmt = "%Y-%m-%d"
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', type=str, required=True)
-    parser.add_argument('--port', type=int, required=True)
-    parser.add_argument('--user', type=str, required=True)
-    parser.add_argument('--password', type=str, required=True)
-    parser.add_argument('--database', type=str, required=True)
-    parser.add_argument('--insert-batch-size', type=int, required=False, default=400000)
-    parser.add_argument('--input-file', type=str, required=True)
-    parser.add_argument('--error-file', type=str, required=False, default="error_records.txt")
+    parser.add_argument("--host", type=str, required=True)
+    parser.add_argument("--port", type=int, required=True)
+    parser.add_argument("--user", type=str, required=True)
+    parser.add_argument("--password", type=str, required=True)
+    parser.add_argument("--database", type=str, required=True)
+    parser.add_argument("--insert-batch-size", type=int, required=True)
+    parser.add_argument("--input-file", type=str, required=True)
+    parser.add_argument("--error-file", type=str, required=True)
     return parser.parse_args()
 
 
 def batch_insert(cursor, entries):
-    inserted_entries = cursor.executemany(
-        """INSERT INTO email_campaigns 
-            (business_id, campaign_date, email, event, campaign_name, sg_event_id, sg_message_id, timestamp)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",
-        entries
-    )
-    print("Inserted %d entries" % inserted_entries)
+    try:
+        stmt = """INSERT INTO email_campaigns
+                (business_id, campaign_date, email, event, campaign_name, sg_event_id, sg_message_id, email_timestamp)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, to_timestamp(%s))"""
+        execute_batch(cursor, stmt, entries)
+        print("Inserted %d entries" % len(entries))
+    except DatabaseError as e:
+        print(e)
 
 
 def validate_row(json_data):
-    return type(json_data['business_id']) == int and \
-           type(json_data['date']) == str and datetime.strptime(json_data['date'], date_fmt) and \
-           type(json_data['email']) == str and len(json_data['email']) > 0 and \
-           type(json_data['event']) == str and len(json_data['event']) > 0 and\
-           type(json_data['campaign_name']) == str and len(json_data['campaign_name']) > 0 and \
-           type(json_data['sg_event_id']) == str and len(json_data['sg_event_id']) > 0 and \
-           type(json_data['sg_message_id']) == str and len(json_data['sg_message_id']) > 0 and\
-           type(json_data['timestamp']) == int and json_data['timestamp'] > 0
+    message = ''
+    if not type(json_data["business_id"]) == int:
+        message += 'business_id must be a number \n'
+
+    if not type(json_data["date"]) == str:
+        message += 'date must be specified as string \n'
+
+    if not datetime.strptime(json_data["date"], date_fmt):
+        message += 'date must be specified in format' + date_fmt + '\n'
+
+    if not (type(json_data["email"]) == str and len(json_data["email"]) > 0):
+        message += 'email must be specified as string and be non empty\n'
+
+    if not (type(json_data["event"]) == str and len(json_data["event"]) > 0):
+        message += 'event must be specified as string and be non empty\n'
+
+    if not (type(json_data["campaign_name"]) == str and len(json_data["campaign_name"]) > 0):
+        message += 'campaign_name must be specified as string and be non empty\n'
+
+    if not (type(json_data["sg_event_id"]) == str and len(json_data["sg_event_id"]) > 0):
+        message += 'sg_event_id must be specified as string and be non empty\n'
+
+    if not (type(json_data["sg_message_id"]) == str and len(json_data["sg_message_id"]) > 0):
+        message += 'sg_message_id must be specified as string and be non empty\n'
+
+    if not (type(json_data["timestamp"]) == int and json_data["timestamp"] > 0):
+        message += 'timestamp must be specified as int and be greater than zero\n'
+
+    return message, message == ''
 
 
 def process_data(db, content):
@@ -53,54 +78,61 @@ def process_data(db, content):
     for data in content:
         try:
             json_data = loads(data)
-            if validate_row(json_data):
-                insert_list.append((
-                    json_data['business_id'],
-                    json_data['date'],
-                    json_data['email'],
-                    json_data['event'],
-                    json_data['campaign_name'],
-                    json_data['sg_event_id'],
-                    json_data['sg_message_id'],
-                    json_data['timestamp']
-                ))
+            message, is_valid = validate_row(json_data)
+            if is_valid:
+                insert_list.append(
+                    (
+                        json_data["business_id"],
+                        json_data["date"],
+                        json_data["email"],
+                        json_data["event"],
+                        json_data["campaign_name"],
+                        json_data["sg_event_id"],
+                        json_data["sg_message_id"],
+                        json_data["timestamp"],
+                    )
+                )
             else:
-                error_list.append(json_data)
+                error_list.append((message, json_data))
         except (JSONDecodeError, ValueError):
-            error_list.append(data)
+            error_list.append(("Improper Json", data))
         if len(insert_list) == args.insert_batch_size:
             batch_insert(cursor, insert_list)
             insert_list = []
     if insert_list:
         batch_insert(cursor, insert_list)
     db.commit()
+    cursor.close()
     return error_list
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = get_args()
-    print('here')
-    print(args)
-    sleep(5)
-    #while True:
-    #    try :
-    db = MySQLdb.connect(host=args.host, user=args.user, passwd=args.password, db=args.database, port=args.port)
-    #        break
-    #    except:
-    #        print("Exception while trying to connect, sleeping and retrying")
-    #        sleep(2)
+    while True:
+        # This happens because its hard to control the order in which containers are brought up
+        try:
+            conn = psycopg2.connect(
+                host=args.host, user=args.user, password=args.password, dbname=args.database, port=args.port
+            )
+            break
+        except Exception as e:
+            print("Exception while trying to connect, retrying in 5 seconds")
+            print(e)
+            sleep(5)
     print("Connection succeeded")
-    '''
-    
+
     with gzip.open(args.input_file, 'rb') as f:
         content = f.readlines()
-    error_list = process_data(db, content)
+    error_list = process_data(conn, content)
 
     if error_list:
         print("There are %d errors, writing to %s" % (len(error_list), args.error_file))
         with open(args.error_file, 'w') as f:
-            for entry in error_list:
+            for message, entry in error_list:
+                f.write("%s\n" % message)
                 f.write("%s\n" % entry)
+                f.write("=====\n")
 
         print("Done writing errors ")
-    '''
+    if conn is not None:
+        conn.close()
--- punchh/db/init.sql
+++ punchh/db/init.sql
@@ -1,14 +1,11 @@
-create database if not exists punchh_test;
-use punchh_test;
-
-create table if not exists email_campaigns (
-  id bigint not null primary key auto_increment,
-  business_id int not null,
-  campaign_date date not null,
-  email varchar(100) not null,
-  event varchar(20) not null,
-  campaign_name varchar(100) not null,
-  sg_event_id varchar(100) not null,
-  sg_message_id varchar(100) not null,
-  timestamp bigint not null
+CREATE TABLE IF NOT EXISTS email_campaigns (
+  id SERIAL PRIMARY KEY,
+  business_id INT NOT NULL,
+  campaign_date DATE NOT NULL,
+  email VARCHAR NOT NULL,
+  event VARCHAR NOT NULL,
+  campaign_name VARCHAR NOT NULL,
+  sg_event_id VARCHAR NOT NULL,
+  sg_message_id VARCHAR NOT NULL,
+  email_timestamp TIMESTAMP NOT NULL
 )
